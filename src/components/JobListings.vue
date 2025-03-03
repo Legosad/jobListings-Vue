7c8555eecd748a3463ce050747cafb347f5dd955
Saad <saadaltaf1202@gmail.com>
2025-03-03 16:16:48 -0500

    starting

---
+++ src/components/JobListings.vue
@@ -0,0 +1,10 @@
+<script setup>
+import jobData from "@/jobsData.json"
+import { ref } from "vue";
+const jobs = ref(jobData)
+    console.log(jobs.value)
+</script>
+<template>
+    <section class></section>
+    <div v-for="job in jobs" :key="job.id">{{ job.title }}</div>
+</template>
